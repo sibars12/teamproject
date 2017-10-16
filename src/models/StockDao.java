93d603ea6fe8b9573db589e943394eb2485918d9
LeeJeondo <LeeJeondo@DESKTOP-ALHFS48>
2017-10-16 14:02:51 +0900

    mapper이름 변경

--- src/models/StockDao.java
+++ src/models/StockDao.java
@@ -10,50 +10,47 @@ public class StockDao {
 	@Autowired
 	SqlSessionTemplate sql;
 	
-	public List getList() {
-		return sql.selectList("product.getList");
-	}
 	public List getStockList(String page) {
-		return sql.selectList("product.getStockList", page);
+		return sql.selectList("stock.getStockList", page);
 	}
 	public int getStockPage() {
-		return sql.selectOne("product.getStockPage");
+		return sql.selectOne("stock.getStockPage");
 	}
 	public List searchStockList(Map map) {
-		return sql.selectList("product.searchStockList", map);
+		return sql.selectList("stock.searchStockList", map);
 	}
 	public int getSearchStockPage(String name) {
-		return sql.selectOne("product.getSearchStockPage", name);
+		return sql.selectOne("stock.getSearchStockPage", name);
 	}
 	public List searchName(String name) {
-		return sql.selectList("product.searchName", name);
+		return sql.selectList("stock.searchName", name);
 	}
 	public boolean addNewStock(Map map) {
-		sql.insert("product.addNewStock", map);
+		sql.insert("stock.addNewStock", map);
 		return true;
 	}
 	public boolean addOldStock(Map map) {
-		sql.insert("product.addOldStock", map);
+		sql.insert("stock.addOldStock", map);
 		return true;
 	}
 	public boolean checkAdd(Map map) {
-		int n = sql.selectOne("product.checkAdd", map);
+		int n = sql.selectOne("stock.checkAdd", map);
 		if(n==0)
 			return true;
 		return false;
 	}
 	public boolean deleteStock(String no) {
-		sql.delete("product.deleteStock", no);
+		sql.delete("stock.deleteStock", no);
 		return true;
 	}
 	public boolean updateStock(Map map) {
-		sql.update("product.updateStock", map);
+		sql.update("stock.updateStock", map);
 		return true;
 	}
 	public List getOwnernumber(Map map) {
-		return sql.selectList("product.getOwnernumber", map);
+		return sql.selectList("stock.getOwnernumber", map);
 	}
 	public List getOwnernumbersubN(Map map) {
-		return sql.selectList("product.getOwnernumbersubN", map);
+		return sql.selectList("stock.getOwnernumbersubN", map);
 	}
 }
